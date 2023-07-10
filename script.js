9402d569e2d8c837798a7b35aa168d19f4627c0f
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2023-07-10 15:29:37 +0200

    Added function init()

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let bundeslaender = [];
 let letters = [];
 
+
+// Die function init() ladt Daten aus JSON Array
 async function init() {
-    let resp = await fetch('')
+    let resp = await fetch('./bundesleand.json');
+    bundeslaender = await resp.json();
+    render();
 }
