9b90eaeb224f5e71a3095dd27f56936d378698c7
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2023-07-10 16:22:21 +0200

    Added function render()

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ async function init() {
     bundeslaender = await resp.json();
     render();
 }
+
+
+// render(filter) ist verantwortlich fur Rendern (Anzeigen) von Inhalten auf einer Webseite.
+function render(filter) {
+    let content = document.getElementById('content');
+    content.innerHTML = '';
+
+    for (let i = 0; i  < bundeslaender.length; i++) {
+        const land = bundeslaender[i];
+        const population = (land['population'] + '').replace('.',',');
+        const firstLetter = land['name'].charAt(0);
+
+        if (!filter || filter == firstLetter) {
+            content.innerHTML += generateLink(land, population);
+        }
+
+        if (!letters.includes(firstLetter)) {
+            letters.push(firstLetter);
+        }
+    }
+    renderLetters();
+}
